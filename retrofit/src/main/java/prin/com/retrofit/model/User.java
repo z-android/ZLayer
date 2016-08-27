c6004232bf80845d43f484ebb3703af6ee4f9e00
z-android <757537413@qq.com>
2016-08-27 10:59:23 +0800

    更新文件

--- retrofit/src/main/java/prin/com/retrofit/model/User.java
+++
@@ -1,7 +0,0 @@
-package prin.com.retrofit.model;
-
-/**
- * Created by prin on 2016/8/25.
- */
-public class User {
-}
--- retrofit/src/main/java/prin/com/retrofit/model/service/BaseRsp.java
+++
@@ -1,7 +0,0 @@
-package prin.com.retrofit.model.service;
-
-/**
- * Created by prin on 2016/8/26.
- */
-public class BaseRsp<T> {
-}
